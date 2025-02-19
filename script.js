71ab2a3c86111851e7d678b004a39bea7f5f06d3
Faraz <farazul.islam@gmail.com>
2025-02-19 15:41:16 +0000

    User input can dynamically change grid size

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 const grid = document.querySelector('.grid');
 const button = document.querySelector('.button')
 
-createGrid(20);
+createGrid(16);
+
+button.addEventListener('click', ()=> {
+    const newGrid = prompt('Please enter a new grid size (not greater than 100)');
+    grid.replaceChildren();
+    createGrid(parseInt(newGrid));
+})
+
 
 function createGrid(number) {
 
     const gridSize = number * number;
 
     for(let i=0; i<gridSize; i++) {
-        let square = document.createElement("div")
-        square.classList.add('gridSquare')
-        square.style.height = `${960/number}px`
-        square.style.width = `${960/number}px`
-        square.addEventListener('mouseover', ()=> square.classList.add('gridSquare-hover'))
+        let square = document.createElement("div");
+        square.classList.add('gridSquare');
+        square.style.height = `${960/number}px`;
+        square.style.width = `${960/number}px`;
+        square.addEventListener('mouseover', ()=> square.classList.add('gridSquare-hover'));
 
         grid.appendChild(square);
     }
